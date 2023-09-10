f11fc2afe4c11e70c97eafc2bc14b33a993500c3
Shantanu <shantanu@thecoderpanda.com>
2023-09-10 15:18:17 +0530

    added final edit

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  // Configure Content Wind here
+  cover: '/cover.jpg',
+  socials: {
+    twitter: 'thecoderpanda',
+    github: 'thecoderpanda/',
+  },
+  prose: {
+    h1: {
+      icon: 'carbon:sailboat-coastal'
+    }
+  }
+})
