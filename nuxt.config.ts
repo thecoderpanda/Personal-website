a4a2ccc807f1cf1cff1bc796be8a575178686ca1
Shantanu Vishwanadha <shantanu@thecoderpanda.com>
2024-06-06 11:19:43 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,33 @@
 export default defineNuxtConfig({
-  extends: 'content-wind'
-})
+  extends: 'content-wind',
+
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://blog.thecoderpanda.com/blog/",
+      },
+      {
+        source: "/blog/(.*?)([.]\\w{2,5})(\\?[^/]+)?",
+        destination: "https://blog.thecoderpanda.com/blog/$1$2$3",
+      },
+      {
+        source: "/blog/([\\S\\s]+)",
+        destination: "https://blog.thecoderpanda.com/blog/$1/",
+      },
+    ];
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/blog/:path*",
+        headers: [
+          { key: "X-Forwarded-Proto", value: "https" },
+          { key: "X-Forwarded-Host", value: thecoderpanda },
+          // { key: "X-Forwarded-For", value: "" },
+        ],
+      },
+    ];
+  },
+});
